5804d6c1790a1c8794aa02ebfd19a1ce58464ffb
wdEffort <yoman3011@gmail.com>
2021-06-30 23:15:31 +0900

    HTTP 요청 데이터 - API Message Body - JSON

--- src/main/java/hello/servlet/basic/request/RequestBodyJsonServlet.java
+++ src/main/java/hello/servlet/basic/request/RequestBodyJsonServlet.java
@@ -1,5 +1,7 @@
 package hello.servlet.basic.request;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import hello.servlet.basic.HelloData;
 import org.springframework.util.StreamUtils;
 
 import javax.servlet.ServletException;
@@ -11,8 +13,11 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 
-@WebServlet(name = "requestBodyStringServlet", urlPatterns = "/request-body-string")
-public class RequestBodyStringServlet extends HttpServlet {
+@WebServlet(name = "requestBodyJsonServlet", urlPatterns = "/request-body-json")
+public class RequestBodyJsonServlet extends HttpServlet {
+
+    // Spring에서 제공하는 JSON 문자열을 객체로 변환하기 위한 라이브러리 설정
+    private ObjectMapper objectMapper = new ObjectMapper();
 
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -20,6 +25,11 @@ public class RequestBodyStringServlet extends HttpServlet {
         String messageBody = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8); // Spring에서 제공하는 Utility를 사용하여 Byte code를 String으로 변환
 
         System.out.println("messageBody = " + messageBody);
+
+        HelloData helloData = objectMapper.readValue(messageBody, HelloData.class); // JSON 문자열을 HelloData class 객체로 변환
+
+        System.out.println("helloData.username = " + helloData.getUsername());
+        System.out.println("helloData.age = " + helloData.getAge());
 
         response.getWriter().write("ok");
     }
