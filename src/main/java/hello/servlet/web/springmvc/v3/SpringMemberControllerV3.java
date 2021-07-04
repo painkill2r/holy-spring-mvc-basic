6d83e0b41c1bd6559587abb0c9754e08c5b7c9f4
wdEffort <yoman3011@gmail.com>
2021-07-04 14:00:55 +0900

    스프링 MVC - 회원관리 웹 애플리케이션 구현(V3 - 실용적인 방식으로 컨트롤러 리팩토링2)

--- src/main/java/hello/servlet/web/springmvc/v3/SpringMemberControllerV3.java
+++ src/main/java/hello/servlet/web/springmvc/v3/SpringMemberControllerV3.java
@@ -4,12 +4,8 @@ import hello.servlet.domain.member.Member;
 import hello.servlet.domain.member.MemberRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
 @Controller
@@ -18,12 +14,14 @@ public class SpringMemberControllerV3 {
 
     private MemberRepository memberRepository = MemberRepository.getInstance();
 
-    @RequestMapping("/new-form")
+    //@RequestMapping(value = "/new-form", method = RequestMethod.GET)
+    @GetMapping("/new-form")
     public String newForm() {
         return "new-form";
     }
 
-    @RequestMapping("/save")
+    //@RequestMapping(value = "/save", method = RequestMethod.POST)
+    @PostMapping("/save")
     public String save(@RequestParam("username") String username,
                        @RequestParam("age") int age,
                        Model model) {
@@ -36,7 +34,8 @@ public class SpringMemberControllerV3 {
         return "save-result";
     }
 
-    @RequestMapping
+    //@RequestMapping(method = RequestMethod.GET)
+    @GetMapping
     public String members(Model model) {
         List<Member> members = memberRepository.findAll();
 
